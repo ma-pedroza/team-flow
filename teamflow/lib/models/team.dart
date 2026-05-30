6760ff254d074e6ee8e2b94da65b95209a404c10
ma-pedroza <matheusgomeszpedroza@gmail.com>
2026-05-30 19:33:49 -0300

    feature: add métodos de SQLite no model

--- teamflow/lib/models/team.dart
+++ teamflow/lib/models/team.dart
@@ -1,8 +1,35 @@
 class Team {
-  final int id;
+  final int? id;
   final String name;
   final String area;
+  final String description;
   final int colorValue;
 
-  Team({required this.id, required this.name, required this.area, required this.colorValue});
+  Team({
+    this.id,
+    required this.name,
+    required this.area,
+    required this.description,
+    required this.colorValue,
+  });
+
+  Map<String, dynamic> toMap() {
+    return {
+      'id': id,
+      'name': name,
+      'area': area,
+      'description': description,
+      'colorValue': colorValue,
+    };
+  }
+
+  factory Team.fromMap(Map<String, dynamic> map) {
+    return Team(
+      id: map['id'],
+      name: map['name'],
+      area: map['area'],
+      description: map['description'],
+      colorValue: map['colorValue'],
+    );
+  }
 }
